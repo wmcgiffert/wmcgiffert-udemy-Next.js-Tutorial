7cbafaeb8ded5defccaee84b961263c22075a855
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-02-14 14:43:55 -0600

    changes to make it production ready

--- ReactAppendix/udemy-appendix-my-first-component/src/Components/CardList.js
+++ ReactAppendix/udemy-appendix-my-first-component/src/Components/CardList.js
@@ -2,12 +2,6 @@ import React from "react";
 import Card from "./Card";
 
 const CardList = ({ robots }) => {
-  if(true) {
-    throw new Error(
-      "Nooooo"
-    );
-  }
-
   return (
     <div>
       {robots.map((user) => {
