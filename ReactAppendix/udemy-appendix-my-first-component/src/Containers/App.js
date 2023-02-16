c475793997e1a61f0d42f417d0d6ae3ac43242b8
William McGiffert <williammcgiffert@Williams-MBP.lan>
2023-02-15 21:11:15 -0600

    added a count clicking button

--- ReactAppendix/udemy-appendix-my-first-component/src/Containers/App.js
+++ ReactAppendix/udemy-appendix-my-first-component/src/Containers/App.js
@@ -9,12 +9,14 @@ import ErrorBoundry from "../Components/ErrorBoundry";
 function App() {
     const [robots, setRobots] = useState([]);
     const [searchfield, setSearchfield] = useState('');
+    const [count, setCount] = useState(0);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => setRobots(users));
-    }, []); 
+        console.log(count);
+    }, [count]); 
 
     const onSearchChange = (event) => {
         setSearchfield(event.target.value);
@@ -23,20 +25,19 @@ function App() {
     const filteredRobots = robots.filter(robot => {
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    console.log(robots, searchfield);
-    if(robots.length === 0) {
-        return <h1 className="tc">Loading</h1>
-    };
-    return(
+
+    return !robots.length ? 
+        <h1 className="tc">Loading</h1>:
+        (
         <div className="tc">
             <h1 className="f1">RoboFriends</h1>
+            <button onClick={()=>setCount(count+1)}>Click Me! The count is {count}</button>
             <SearchBox searchChange = {onSearchChange} />
             <Scroll>
                 <ErrorBoundry>
                     <CardList robots={filteredRobots} />
                 </ErrorBoundry>
             </Scroll>
-
         </div>
     );
 }
