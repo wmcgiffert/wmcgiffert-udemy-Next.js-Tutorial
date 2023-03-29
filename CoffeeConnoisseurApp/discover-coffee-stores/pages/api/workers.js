4332342e58cb9ec66de14b35ceb687a640c8aa56
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-03-29 14:10:52 -0500

    updated /[id].js to remove the warn quick refresh error

--- CoffeeConnoisseurApp/discover-coffee-stores/pages/api/workers.js
+++ CoffeeConnoisseurApp/discover-coffee-stores/pages/api/workers.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+export default function Handler(req, res) {
   const currentDate = new Date(Date.now());
   const workers = {
     messsage: 'Last updated: ' + currentDate,
