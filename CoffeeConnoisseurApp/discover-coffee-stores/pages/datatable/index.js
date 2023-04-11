1c91815d2b435cf135a0ccfc6362e6a5e374584a
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-11 16:00:35 -0500

    Added Button not sure if want to use

--- CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
+++ CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
@@ -1,44 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import DataTable from 'react-data-table-component';
 import styles from '@/styles/datatable.module.css'
 import Image from 'next/image';
 import logo from '../../public/static/ABlogo.png'
 import SearchBox from '@/components/datatableComponents/SearchBox'
 
+
 const DataTablePage = () => {
     const tableData = [
-        { id: 1, model: 'FundInfo', property: 28, value: 'New York'},
-        { id: 2, model: 'FundInfo', property: 32, value: 'San Francisco' },
-        { id: 3, model: 'FundInfo', property: 24, value: 'Los Angeles' },
-        { id: 4, model: 'FundFilter', property: 31, value: 'Chicago' },
-        { id: 5, model: 'FundFilter', property: 32, value: 'Nashville' },
-        { id: 6, model: 'FundFilter', property: 33, value: 'Boston' },
-        { id: 7, model: 'FundFilter', property: 34, value: 'Atlanta' },
-        { id: 8, model: 'FundInfo', property: 35, value: 'Birmingham' },
-        { id: 9, model: 'FundInfo', property: 36, value: 'Charlotte' },
-        { id: 10, model: 'KeyFacts', property: 30, value: 'Denver' },
-        { id: 11, model: 'KeyFacts', property: 30, value: 'Charleston' },
-        { id: 12, model: 'KeyFacts', property: 30, value: 'New Mexico City' },
-        { id: 13, model: 'KeyFacts', property: 30, value: 'Dallas' },
-        { id: 14, model: 'KeyFacts', property: 30, value: 'Miami' },
-    ];
+        {id: 1, model: 'FundInfo', property: 28, default: 'New York'},
+        {id: 2, model: 'FundInfo', property: 32, default: 'San Francisco' },
+        {id: 3, model: 'FundInfo', property: 24, default: 'Los Angeles' },
+        {id: 4, model: 'FundFilter', property: 31, default: 'Chicago' },
+        {id: 5, model: 'FundFilter', property: 32, default: 'Nashville' },
+        {id: 6, model: 'FundFilter', property: 33, default: 'Boston' },
+        {id: 7, model: 'FundFilter', property: 34, default: 'Atlanta' },
+        {id: 8, model: 'FundInfo', property: 35, default: 'Birmingham' },
+        {id: 9, model: 'FundInfo', property: 36, default: 'Charlotte' },
+        {id: 10, model: 'KeyFacts', property: 25, default: 'Denver' },
+        {id: 11, model: 'KeyFacts', property: 26, default: 'Charleston' },
+        {id: 12, model: 'KeyFacts', property: 27, default: 'New Mexico City' },
+        {id: 13, model: 'KeyFacts', property: 29, default: 'Dallas' },
+        {id: 14, model: 'KeyFacts', property: 20, default: 'Miami' },
+    ] 
     const columns = [
-        // { name: 'ID', selector: row => row.id, sortable: true },
+        {
+            cell: () => <button onClick={handleButtonClick} className={styles.button}> Edit </button>,
+            ignoreRowClick: true, 
+            allowOverflow: true, 
+            button: true,
+        },
         { name: 'Model', selector: row => row.model, sortable: true },
         { name: 'Property', selector: row => row.property, sortable: true },
-        { name: 'Value', selector: row => row.value, sortable: true },
-        { name: 'EN-GB', selector: row => row.value, sortable: true },
-        { name: 'UK-HG', selector: row => row.value, sortable: true },
-        { name: 'SP', selector: row => row.value, sortable: true },
-        // { name: 'Edit', selector: row => row.edit, sortable: false}
+        { name: 'Default', selector: row => row.default, sortable: true },
+        { name: 'EN-GB', selector: row => row.default, sortable: true },
+        { name: 'UK-HG', selector: row => row.default, sortable: true },
+        { name: 'SP', selector: row => row.default, sortable: true },
+        // { name: 'ID', selector: row => row.id, sortable: true },
     ];
     const listWithKey = tableData.map(record => (
         {
             id:`${record.id}`,
-            transKey:`${record.model}-${record.property}-${record.value}`,
+            transKey:`${record.model}-${record.property}-${record.default}`,
             model:`${record.model}`,
             property:`${record.property}`,
-            value:`${record.value}`,
+            default:`${record.default}`,
         }
     ));
 
@@ -50,9 +56,15 @@ const DataTablePage = () => {
     const onSearchChange = (event) => {
         setSearchfield(event.target.value);
     };
+
     const filteredTranslations = data.filter( translation => {
         return translation.transKey.toLowerCase().includes(searchfield.toLowerCase());
     });
+
+    const handleButtonClick = () => {
+		
+		console.log('edit');
+	};
 
   return (
     <div className={styles.container}>
@@ -63,6 +75,7 @@ const DataTablePage = () => {
                 src={logo}
                 className={styles.img}
                 alt='AB Logo'
+                priority
             />
             <div className={styles.title}>
                 AllianceBernstien
@@ -79,7 +92,6 @@ const DataTablePage = () => {
                 data={filteredTranslations}
                 pagination
                 selectableRows
-                
             />
         </div>
     </div>
