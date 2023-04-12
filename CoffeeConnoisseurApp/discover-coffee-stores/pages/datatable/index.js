efe598965e4c126fa6c33c25d5b62e9a16816118
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-12 13:34:41 -0500

    Makeing the datatable editable

--- CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
+++ CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
@@ -7,6 +7,7 @@ import SearchBox from '@/components/datatableComponents/SearchBox'
 
 
 const DataTablePage = () => {
+    //Data
     const tableData = [
         {id: 1, model: 'FundInfo', property: 28, default: 'New York'},
         {id: 2, model: 'FundInfo', property: 32, default: 'San Francisco' },
@@ -23,21 +24,53 @@ const DataTablePage = () => {
         {id: 13, model: 'KeyFacts', property: 29, default: 'Dallas' },
         {id: 14, model: 'KeyFacts', property: 20, default: 'Miami' },
     ] 
+    //Table structure rules
     const columns = [
-        {
-            cell: () => <button onClick={handleButtonClick} className={styles.button}> Edit </button>,
-            ignoreRowClick: true, 
-            allowOverflow: true, 
-            button: true,
-        },
         { name: 'Model', selector: row => row.model, sortable: true },
         { name: 'Property', selector: row => row.property, sortable: true },
         { name: 'Default', selector: row => row.default, sortable: true },
-        { name: 'EN-GB', selector: row => row.default, sortable: true },
-        { name: 'UK-HG', selector: row => row.default, sortable: true },
-        { name: 'SP', selector: row => row.default, sortable: true },
-        // { name: 'ID', selector: row => row.id, sortable: true },
+        // { name: 'EN-GB', selector: row => row.default, sortable: true },
+        // { name: 'UK-HG', selector: row => row.default, sortable: true },
+        // { name: 'SP', selector: row => row.default, sortable: true },
+        {
+            name: 'TW',
+            cell: (rowData) => (
+                <EditCell
+                    rowData={rowData}
+                    column={{name: 'TW', selector: row => row.default, sortable: true}}
+                    updateData={updateData}
+                />
+            )
+
+        },
+
     ];
+
+    //Edit the cell function
+    const EditCell = ({ row, column, updateData }) =>{
+        const [ isEditable, setIsEditable ] = useState(false);
+        const [ cellValue, setCellValue ] = useState(rowData[column.selector]);
+        
+        const handleSave = () => {
+            setIsEditable(false)
+            updateData(rowData.id , column.selector, cellValue);
+
+            return isEditable ? (
+                <input
+                    value={cellValue}
+                    onChange={(cell)=> setCellValue(cell.target.cellValue)}
+                    onBlur={handleSave}
+                    onKeyDown={(press)=> press.key === 'Enter' && handleSave()}
+                />
+            ):(
+                <span onClick={()=> setIsEditable(true)}>
+                    {rowData[column.selector]}
+                </span>
+            );
+        };
+    };
+
+    //Makes a translationKey for the from the orginal data
     const listWithKey = tableData.map(record => (
         {
             id:`${record.id}`,
@@ -48,23 +81,29 @@ const DataTablePage = () => {
         }
     ));
 
+    //state for data and the searchbox
     const [data, setData] = useState([]);
     const [searchfield, setSearchfield] = useState('');
-
+    
+    //updates data with new data with translationKey data
     useEffect(()=>setData(listWithKey),[]);
-
+    
+    //searchbox entered into the text and filter by text change
     const onSearchChange = (event) => {
         setSearchfield(event.target.value);
     };
-
     const filteredTranslations = data.filter( translation => {
         return translation.transKey.toLowerCase().includes(searchfield.toLowerCase());
     });
-
-    const handleButtonClick = () => {
-		
-		console.log('edit');
-	};
+    
+    //update edited data
+    useEffect((data)=> {
+        const [] id, field, value ] = data
+        const newData = data.map((row)=>
+            row.id === id ?{ ...row, [field]: value} : row
+        );
+        setData(newData);
+    },[])
 
   return (
     <div className={styles.container}>
@@ -90,8 +129,7 @@ const DataTablePage = () => {
                 title="Translation Records"
                 columns={columns}
                 data={filteredTranslations}
-                pagination
-                selectableRows
+
             />
         </div>
     </div>
