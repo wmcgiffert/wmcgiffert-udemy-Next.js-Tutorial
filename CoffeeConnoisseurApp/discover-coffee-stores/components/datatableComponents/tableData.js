388311f0621bd9de94376537f9b75c36354cabe3
wmcgiffert <wmcgiffert@crimson.ua.edu>
2023-04-13 12:56:14 -0500

    Using different React Tables instead of ReactDataTables

---
+++ CoffeeConnoisseurApp/discover-coffee-stores/components/datatableComponents/tableData.js
@@ -0,0 +1,18 @@
+const tableData = [
+        {id: 1, model: 'FundInfo', property: 28, default: 'New York'},
+        {id: 2, model: 'FundInfo', property: 32, default: 'San Francisco' },
+        {id: 3, model: 'FundInfo', property: 24, default: 'Los Angeles' },
+        {id: 4, model: 'FundFilter', property: 31, default: 'Chicago' },
+        {id: 5, model: 'FundFilter', property: 32, default: 'Nashville' },
+        {id: 6, model: 'FundFilter', property: 33, default: 'Boston' },
+        {id: 7, model: 'FundFilter', property: 34, default: 'Atlanta' },
+        {id: 8, model: 'FundInfo', property: 35, default: 'Birmingham' },
+        {id: 9, model: 'FundInfo', property: 36, default: 'Charlotte' },
+        {id: 10, model: 'KeyFacts', property: 25, default: 'Denver' },
+        {id: 11, model: 'KeyFacts', property: 26, default: 'Charleston' },
+        {id: 12, model: 'KeyFacts', property: 27, default: 'New Mexico City' },
+        {id: 13, model: 'KeyFacts', property: 29, default: 'Dallas' },
+        {id: 14, model: 'KeyFacts', property: 20, default: 'Miami' },
+];
+
+export default tableData;
--- CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
+++ CoffeeConnoisseurApp/discover-coffee-stores/pages/datatable/index.js
@@ -4,25 +4,13 @@ import styles from '@/styles/datatable.module.css'
 import Image from 'next/image';
 import logo from '../../public/static/ABlogo.png'
 import SearchBox from '@/components/datatableComponents/SearchBox'
+import tableData from '@/components/datatableComponents/tableData';
+
 
 
 const DataTablePage = () => {
-    const tableData = [
-        {id: 1, model: 'FundInfo', property: 28, default: 'New York'},
-        {id: 2, model: 'FundInfo', property: 32, default: 'San Francisco' },
-        {id: 3, model: 'FundInfo', property: 24, default: 'Los Angeles' },
-        {id: 4, model: 'FundFilter', property: 31, default: 'Chicago' },
-        {id: 5, model: 'FundFilter', property: 32, default: 'Nashville' },
-        {id: 6, model: 'FundFilter', property: 33, default: 'Boston' },
-        {id: 7, model: 'FundFilter', property: 34, default: 'Atlanta' },
-        {id: 8, model: 'FundInfo', property: 35, default: 'Birmingham' },
-        {id: 9, model: 'FundInfo', property: 36, default: 'Charlotte' },
-        {id: 10, model: 'KeyFacts', property: 25, default: 'Denver' },
-        {id: 11, model: 'KeyFacts', property: 26, default: 'Charleston' },
-        {id: 12, model: 'KeyFacts', property: 27, default: 'New Mexico City' },
-        {id: 13, model: 'KeyFacts', property: 29, default: 'Dallas' },
-        {id: 14, model: 'KeyFacts', property: 20, default: 'Miami' },
-    ] 
+    console.table(tableData);
+    
     const columns = [
         {
             cell: () => <button onClick={handleButtonClick} className={styles.button}> Edit </button>,
